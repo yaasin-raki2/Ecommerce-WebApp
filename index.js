89a18dd98cf0a0d753c67d2457bd6117e02928f8
Yassine <yassin23raki@gmail.com>
2020-10-14 17:00:42 +0100

    quick demo

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-console.log('test test *-*');
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send(`
+        <div>
+            <form method="POST">
+                <input name="email" placeholder="email">
+                <input name="password" placeholder="password">
+                <input name="passwordConfirmation" placeholder="password Confirmation">
+                <button>Sign up</button>
+            </form>
+        </div>
+    `);
+})
+
+const bodyParser = (req, res, next) => {
+    if (req.method === "POST") {
+        req.on('data', data => {
+            const parsed = data.toString('utf8').split('&');
+            const formData = {}
+            for (let pair of parsed) {
+                const [key, value] = pair.split('=');
+                formData[key] = value;
+            }
+            req.body = formData;
+            next();
+        });
+    } else {
+        next();
+    }
+
+}
+
+app.post('/', bodyParser, (req, res) => {
+    console.log(req.body);
+    res.send('Account created')
+})
+
+app.listen(3000, () => {
+    console.log('Listening...');
+})
