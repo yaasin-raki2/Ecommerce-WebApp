4c112c9b7e25104e2c5de96373e656aed200ca72
Yassine <yassin23raki@gmail.com>
2020-10-18 20:18:45 +0100

    creating an admin's products page

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const { validationResult } = require('express-validator');
 
+const { handleErrors } = require('./middlewares');
 const usersRepo = require('../../repositories/users');
 const signupTemplate = require('../../views/admin/auth/signup');
 const signinTemplate = require('../../views/admin/auth/signin');
@@ -18,20 +18,17 @@ router.get("/signup", (req, res) => {
     res.send(signupTemplate({ req }));
 });
  
-router.post("/signup", [ requireEmail, requirePassword, requirePasswordConfirmation ], async (req, res) => {
+router.post(
+    "/signup", 
+    [ requireEmail, requirePassword, requirePasswordConfirmation ],
+    handleErrors(signupTemplate),
+    async (req, res) => {
 
-    const errors = validationResult(req);
-    
-    if (!errors.isEmpty()) {
-        return res.send(signupTemplate({ req, errors }));
-    }
-
-    const { email, password, passwordConfirmation } = req.body;
+    const { email, password } = req.body;
     const user = await usersRepo.create({ email, password });
-
     req.session.userId = user.id;
 
-    res.send("Account created");
+    res.redirect('/admin/products');
 });
 
 router.get("/signout", (req, res) => {
@@ -43,19 +40,17 @@ router.get("/signin", (req, res) => {
     res.send(signinTemplate({}));
 });
 
-router.post('/signin', [ requireEmailExists, requireValidPasswordForUser], async (req, res) => {
-
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()) {
-        return res.send(signinTemplate({ errors }));
-    }
+router.post(
+    '/signin', 
+    [ requireEmailExists, requireValidPasswordForUser], 
+    handleErrors(signinTemplate),
+    async (req, res) => {
 
     const { email } = req.body;
     const user = await usersRepo.getOneBy({ email });
-
     req.session.Id = user.id;
-    res.send('you are signed in !!!');
+
+    res.redirect('/admin/products');
 })
 
 module.exports = router;
---
+++ routes/admin/middlewares.js
@@ -0,0 +1,15 @@
+const { validationResult } = require('express-validator');
+
+module.exports = {
+    handleErrors (templateFunc) {
+        return (req, res, next) => {
+            const errors = validationResult(req);
+
+            if (!errors.isEmpty()) {
+                res.send(templateFunc({ errors }));
+            }
+
+            next();
+        }
+    }
+}
