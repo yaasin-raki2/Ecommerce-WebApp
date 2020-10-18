dc9dc57ffa2a8eb5d11250928e465b5e75dc23f2
Yassine <yassin23raki@gmail.com>
2020-10-18 13:47:29 +0100

    Expanding the Data Store & adding the functionality to create a new a product for the admin

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cookieSession = require("cookie-session");
 const authRouter = require('./routes/admin/auth.js');
+const productsRouter = require('./routes/admin/products');
 
 const app = express();
 
@@ -9,6 +10,7 @@ app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieSession({ keys: ["iusdyf56kqdsjdfqskjd4cyj6è"] }));
 app.use(authRouter);
+app.use(productsRouter);
 
 app.listen(3000, () => {
     console.log("Listening...");
