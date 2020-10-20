5e11b62d1d85a1af7c0432b7afe7d25a1e30fa23
Yassine <yassin23raki@gmail.com>
2020-10-20 14:57:43 +0100

    adding the delete functionality to the cart page

--- routes/carts.js
+++ routes/carts.js
@@ -27,7 +27,7 @@ router.post('/cart/products', async (req, res) => {
 
     await cartsRepo.update(cart.id, { items: cart.items });
 
-    res.send('product added to cart');
+    res.redirect('/cart');
 
 });
 
@@ -48,6 +48,17 @@ router.get('/cart', async (req, res) => {
     res.send(cartShowTemplate({ items: cart.items }));
 })
 
+router.post('/cart/products/delete', async (req, res) => {
+    
+    const { itemId } = req.body;
 
+    const cart = await cartsRepo.getOne(req.session.cartId);
+
+    const items = cart.items.filter( item => item.id !== itemId );
+
+    await cartsRepo.update(req.session.cartId, { items });
+
+    res.redirect('/cart');
+})
 
 module.exports = router;
