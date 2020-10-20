c7b6fe779d148e6bdcc9ffbafc7cf0126f3ba995
Yassine <yassin23raki@gmail.com>
2020-10-20 02:24:51 +0100

    Creating a Shoping Cart System

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cookieSession = require("cookie-session");
 const authRouter = require('./routes/admin/auth');
 const adminProductsRouter = require('./routes/admin/products');
 const productsRouter = require('./routes/products');
+const cartsRouter = require('./routes/carts');
 
 const app = express();
 
@@ -13,6 +14,7 @@ app.use(cookieSession({ keys: ['lkasld235j'] }));
 app.use(authRouter);
 app.use(adminProductsRouter);
 app.use(productsRouter);
+app.use(cartsRouter);
 
 app.listen(3000, () => {
     console.log("Listening...");
